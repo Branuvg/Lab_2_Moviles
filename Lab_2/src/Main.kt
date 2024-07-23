7e253a8b4e178ee68bbfa7f4d89ca30c291f0a20
Gabriel Bran <bra23590@uvg.edu.gt>
2024-07-23 08:52:46 -0600

    Terminado

--- Lab_2/src/Main.kt
+++ Lab_2/src/Main.kt
@@ -1,14 +1,72 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+fun calculateProduct(numbers: List<Int>): Int {
+    return numbers.reduce { acc, num -> acc * num}
+}
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+fun calcularPromedio(numeros: List<Int>): Int {
+    return numeros.reduce { acc, num -> acc + num } / numeros.size
+}
+
+fun filter(numeros: List<Int>): List<Int> {
+    return numeros.filter { it % 2 != 0 }
+}
+
+fun isPalindrome(str: String): Boolean = str.toLowerCase() == str.toLowerCase().reversed()
+
+fun agregarSaludos(nombres: List<String>): List<String> {
+    return nombres.map {"¡Hola, $it!"}
+}
+
+fun performOperation(a: Int, b: Int, operation: (Int, Int) -> Int): Int {
+    return operation(a, b)
+}
+
+data class Person(val name: String, val age: Int, val gender: String)
+data class Student(val name: String, val age: Int, val gender: String, val studentId: String)
+
+fun mapPersonToStudent(persons: List<Person>): List<Student> {
+    return persons.map { person -> Student(person.name, person.age, person.gender, "S${person.name.take(3)}${person.age}") }
+}
+
+fun printStudents(students: List<Student>) {
+    students.forEach { student ->
+        println("El Estudiante ${student.name} tiene ${student.age}")
     }
+}
+
+fun main() {
+    val nums = listOf(1, 2, 3, 4, 5, 6, 7, 8)
+    val prom = calcularPromedio(nums)
+    println("El promedio es: $prom")
+    println("")
+
+
+    val enteros = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    val impares = filter(enteros)
+    println("Números impares: $impares")
+    println("")
+
+    val palabra = "Hannah"
+    val isPal = isPalindrome(palabra)
+    println(isPal)
+    println("")
+
+    val nombres = listOf("Gabriel", "Bran", "Bolaños")
+    val saludos = agregarSaludos(nombres)
+    println(saludos)
+    println("")
+
+    val suma = performOperation(2, 3) { x, y -> x + y }
+    val resta = performOperation(10, 9) { x, y -> x - y }
+    val mult = performOperation(5, 3) { x, y -> x * y }
+    val div = performOperation(10, 2) { x, y -> x / y }
+    println("Suma: $suma, Resta: $resta, Multiplicción: $mult, Division: $div")
+    println("")
+
+    val persons = listOf(
+        Person("David", 20, "Masculino"),
+        Person("Juan", 22, "Masculino"),
+        Person("Ana", 12, "Femenino")
+    )
+    val students = mapPersonToStudent(persons)
+    printStudents(students)
 }
